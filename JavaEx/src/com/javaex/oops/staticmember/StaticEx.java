d48cb683b10bd03b8b717b2a9757d99297613e3d
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-03-26 12:40:04 +0900

    24.03.26 12:40

--- JavaEx/src/com/javaex/oops/staticmember/StaticEx.java
+++ JavaEx/src/com/javaex/oops/staticmember/StaticEx.java
@@ -1,5 +1,38 @@
 package com.javaex.oops.staticmember;
 
 public class StaticEx {
+	public static int refCount; 	// 클래스 변수
+	// -> 생성된 모든 인스턴스의 갯수
+	public static String classVar; 	// 클래스 변수
+	public String instanceVar;		// 인스턴스 변수
+	
+	// 스태틱 블록으로 클래스 변수 초기화
+	
+	static {
+		refCount = 0;	// 스태틱 변수 초기화
+		classVar = "Static Member";	// 클래스 변수 접근
+		// instanceVar = "Instance Member";	// 스태틱 영역에서 인스턴스 변수로의 접근 불가.
+		// static 영역에서는 static 영역만 접근 가능
+		System.out.println("클래스 영역 초기화");
+	}
+	
+	// 생성자
+	public StaticEx() {
+		// 인스턴스가 생성될 때, 객체 카운트를 증가
+		refCount ++;
+		//System.out.println("refCount:" + refCount);
+		System.out.println("Instance 생성!");
+	}
+	
+	// 소멸자
+	// 자바에서 소멸자는 마음대로 불리는게 아닌.. 가비지 컬렉터가 알아서 하는거니까 권장하지 않는듯해
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		refCount--;
+		// 객체가 소멸 될 때 참조 카운트 1개 줄임
+		super.finalize();
+		System.out.println("소멸자 호출");
+	}
 
 }
--- JavaEx/src/com/javaex/oops/staticmember/StaticExApp.java
+++ JavaEx/src/com/javaex/oops/staticmember/StaticExApp.java
@@ -1,11 +1,30 @@
 package com.javaex.oops.staticmember;
 
 public class StaticExApp {
-	public static int refCount; 	// 클래스 변수
-	public static String classVar; 	// 클래스 변수
-	public String instanceVar;		// 인스턴스 변수
 	
 	public static void main(String[] args) {
-	
+		StaticEx[] statics = new StaticEx[3];
+		
+		statics[0] = new StaticEx();
+		System.out.println("refCount:" + statics[0].refCount);
+
+		statics[1] = new StaticEx();
+		System.out.println("refCount:" + StaticEx.refCount);
+		
+		statics[2] = new StaticEx();
+		System.out.println("refCount:" + StaticEx.refCount);
+		
+		statics[2] = null;
+		System.out.println("s3 해제");
+		System.out.println("refCount:" + StaticEx.refCount);
+		System.gc(); // 가비지 컬렉터 호출
+		
+		try {
+			Thread.sleep(3000);	// 3초 대기
+			System.out.println("refCount:" + StaticEx.refCount);
+		} 
+		catch (Exception e) {
+
+		}
 	}
 }
