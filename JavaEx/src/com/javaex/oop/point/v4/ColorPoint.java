599a3fb1a11195b6683e2ca08150db4d90549df7
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-03-27 12:02:15 +0900

    12:02

--- JavaEx/src/com/javaex/oop/point/v4/ColorPoint.java
+++ JavaEx/src/com/javaex/oop/point/v4/ColorPoint.java
@@ -4,16 +4,26 @@ package com.javaex.oop.point.v4;
 public class ColorPoint extends Point{
 	private String color;
 	
+	// 생성자
 	public ColorPoint(String color) {
+		// 생성자 내에서 부모 생성자를 호출하지 않으면 X
 		super(0, 0);
 		this.color = color;
 	}
 
 	public ColorPoint(int x, int y, String color) {
-		super(x, y);
+		super(x, y); // 부모 생성자 호출
 		this.color = color;
 	}
 	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
 	@Override
 	public void draw() {
 		super.draw();
