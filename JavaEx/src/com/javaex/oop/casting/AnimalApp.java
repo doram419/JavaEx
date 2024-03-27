5e2a5adc18676f9a15d55029a4fcc7384f500005
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-03-27 16:01:44 +0900

    16:01

--- JavaEx/src/com/javaex/oop/casting/AnimalApp.java
+++ JavaEx/src/com/javaex/oop/casting/AnimalApp.java
@@ -5,5 +5,10 @@ public class AnimalApp {
 		Animal dog1 = new Dog("Snoopy");
 		// Upcasting : 부모 타입으로 자손 객체를 참조하는 것
 		// 참조 타입에 설계된 것만 사용할 수 있다.
+		
+		dog1.eat();
+		dog1.walk();
+//		dog1.bark();
+		// 부모 설계도에 있는 것만 사용가능
 	}
 }
